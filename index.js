bbdd6418aa4c25a5a0f216385ec76879ce65df4f
David Requeno <davidrequeno52@gmail.com>
2024-03-13 21:32:18 -0600

    rutas

--- index.js
+++ index.js
@@ -3,8 +3,34 @@ import express from 'express'
 const app = express()
 const PORT = process.env.PORT ?? 3000
 
-app.get('/', (req, res) => {
-  res.send('Hola')
+// Obtener todas las ofertas de trabajo
+app.get('/jobs', (req, res) => {
+  res.send('jobs')
+})
+
+// Obtener una oferta de trabajo por ID
+app.get('/jobs/:id', (req, res) => {
+  res.send('jobs por id')
+})
+
+// Buscar ofertas de trabajo por criterios específicos
+app.get('/jobs/search', (req, res) => {
+  res.send('busqueda')
+})
+
+// Crear una nueva oferta de trabajo
+app.post('/jobs', (req, res) => {
+  res.send(' crear una nueva oferta de trabajo')
+})
+
+// Actualizar una oferta de trabajo existente
+app.patch('/jobs/:id', (req, res) => {
+  res.send('actualizar')
+})
+
+// Eliminar una oferta de trabajo existente
+app.delete('/jobs/:id', (req, res) => {
+  res.send('eliminar')
 })
 
 app.listen(PORT, () => {
